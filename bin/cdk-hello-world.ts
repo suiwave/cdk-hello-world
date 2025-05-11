7f1f5e6f2e2240087fbe219e6720dc9ed872e01c
suiwave <184898096+suiwave@users.noreply.github.com>
2025-05-11 11:17:35 +0900

    Initial commit

---
+++ bin/cdk-hello-world.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { CdkHelloWorldStack } from '../lib/cdk-hello-world-stack';
+
+const app = new cdk.App();
+new CdkHelloWorldStack(app, 'CdkHelloWorldStack');
